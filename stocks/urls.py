c20913cfd1a2a8757bc7bcc17b5ae06a62a4e4f5
Mariano <m.fragueiro.lazcano@gmail.com>
2024-06-19 10:21:26 -0300

    add url to login

--- stocks/urls.py
+++ stocks/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import SignUpView, StockInfoView
+from .views import SignUpView, StockInfoView, LoginView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('login/', LoginView.as_view(), name='login'),
     path('stock/', StockInfoView.as_view(), name='stock-info'),
 ]
