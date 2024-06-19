8f0fd2d24059283ae7f5e2ab8bb4b7e675dfac5a
Mariano <m.fragueiro.lazcano@gmail.com>
2024-06-18 22:17:53 -0300

    add last day variation and requirements

--- stocks/views/StockView.py
+++ stocks/views/StockView.py
@@ -20,21 +20,16 @@ class StockInfoView(generics.CreateAPIView):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         symbol = serializer.validated_data.get('symbol')
+        function = serializer.validated_data.get('function', 'TIME_SERIES_DAILY_ADJUSTED')
 
-        
-        # Construir la URL de solicitud
-        params = f'query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={api_key}'
+        params = f'query?function={function}&symbol={symbol}&apikey={api_key}'
         url = f'{base_url}{params}'
+
         url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=IBM&apikey=demo'
 
-        # if time_period:
-        #     url += f'&time_period={time_period}'
-        # if series_type:
-        #     url += f'&series_type={series_type}'
-
-        # Realizar la solicitud a Alpha Vantage
         response = requests.get(url)
         data = response.json()
+        data = self.manage_response(data)
 
         if 'Error Message' in data:
             return Response({"error": data['Error Message']}, status=status.HTTP_400_BAD_REQUEST)
@@ -49,3 +44,15 @@ class StockInfoView(generics.CreateAPIView):
         return Response({'error': str(exc)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     
 
+    def manage_response(self, data):
+        time_series = data.get("Time Series (Daily)", {})
+        dates = sorted(time_series.keys(), reverse=True)
+        if len(dates) < 2:
+            return Response({"error": "Not enough data points to calculate variation."}, status=status.HTTP_400_BAD_REQUEST)
+
+        latest_close = float(time_series[dates[0]]["4. close"])
+        previous_close = float(time_series[dates[1]]["4. close"])
+        variation = latest_close - previous_close
+
+        data['Meta Data']['last day variation'] = variation
+        return data
