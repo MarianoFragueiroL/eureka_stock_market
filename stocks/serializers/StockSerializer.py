d128099074b46eae92dc9bc8379b2a089fc16f7e
Mariano <m.fragueiro.lazcano@gmail.com>
2024-06-18 22:06:38 -0300

    update serilizer

--- stocks/serializers/StockSerializer.py
+++ stocks/serializers/StockSerializer.py
@@ -2,3 +2,4 @@ from rest_framework import serializers
 
 class StockRequestSerializer(serializers.Serializer):
     symbol = serializers.CharField(max_length=10)
+    function = serializers.CharField(max_length=50, required=False)
